29a4fe99a2f62f22f3a8d15b81904cbf101e5864
Niektóry <niektory@gmail.com>
2018-02-20 01:45:37 +0100

    Add incident cards

--- scripts/incident.py
+++ scripts/incident.py
@@ -5,4 +5,10 @@ from deck import Deck
 
 class IncidentDeck(Deck):
 	def __init__(self):
-		pass
+		Deck.__init__(self)
+		self.deck = ["crisis of faith", "crossing to Higan", "endless party", "eternal night",
+			"five impossible requests", "great barrier weakening", "great fairy wars",
+			"Lily White", "overdrive", "rekindle blazing hell", "Saigyou Ayakashi blooming",
+			"scarlet weather rhapsody", "spring snow", "undefined fantastic object",
+			"voyage to Makai", "worldly desires"]
+		self.shuffle()
